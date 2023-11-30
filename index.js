507917e26b07bd45616db3b81483743f20bbfc6c
Connor Persels <48804833+Troidbit@users.noreply.github.com>
2023-11-30 16:47:34 -0600

    server things
    see ya saturday

--- index.js
+++ index.js
@@ -3,11 +3,127 @@ const app = express();
 const cors = require('cors');
 
 app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded());
 
+// GETSSSSSS //
 app.get('/', (req, res) => {
     res.send('Hello from our server!')
+
+    // req.status = 200;
 })
 
-app.listen(8080, () => {
-    console.log('server listening on port 8080')
+app.get('/allusers', (req,res) => {
+
 })
+
+// POSTSSSSSSS //
+app.post('/createpost',(req,res) => {
+    let content = body.content
+
+
+    //tell mongodb to start cooking
+
+
+    res.status = 201;
+})
+
+/*
+app.post('/createcomment',(req,res) => {
+
+    //get object
+    //tell mongodb to start cooking
+    res.status = 201;
+})
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// vv PORT THINGY IS THIS WAY vv
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//WAY
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//WAY WAY
+
+
+
+
+
+
+
+
+
+
+
+
+
+//WAY DOWN 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// HERE
+app.listen(8080, () => { console.log('server listening on port 8080') })
